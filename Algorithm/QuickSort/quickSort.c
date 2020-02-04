7276de33b7dcbdd512f29900661cfbec08b6c9c1
KHM <chfhr0202@gamil.com>
2020-02-04 16:03:54 +0900

    2020.02.04-MyP

---
+++ Algorithm/QuickSort/quickSort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void quickSort(int *data, int start, int end){
+	if (start>=end){
+		return;
+	}
+	
+	int key = start;
+	int i = start + 1;
+	int j = end;
+	int temp;
+	
+	while(i<=j){
+		while(data[i]<=data[key]){
+			i++;
+		}
+		while(data[j]>=data[key] && j > start){
+			j--;
+		}
+		
+		if(i > j){
+			temp = data[j];
+			data[j] = data[key];
+			data[key] = temp;
+		}else {
+			temp = data[j];
+			data[j] = data[i];
+			data[i] = temp;
+		}
+	}
+	
+	quickSort(data,start,j-1);
+	quickSort(data,j+1,end);
+}
+
+void main(void){
+	
+	int data[10] = {1,10,5,8,7,6,4,3,2,9};
+	int number = 10;
+	
+	quickSort(data,0,number-1);
+	
+	int i;
+	
+	for(i=0 ; i < number ; i++){
+		printf(" %d ",data[i]);
+	}
+}
